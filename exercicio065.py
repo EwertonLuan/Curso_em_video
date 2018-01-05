38f1d1271b6ea2275e7fca7b442161006b2ba5e6
Ewerton <ewerton.luan@outlook.com>
2018-01-04 23:33:53 -0200

    Merge branch 'master' of https://github.com/EwertonLuan/Curso_em_video
    # Conflicts:
    #	exercicio061.py

--- exercicio065.py
+++ exercicio065.py
@@ -1,38 +1,40 @@
 
-c = ''
-repete = ''
-while not 'N' in repete:
-    while not 'N' in c:
-        primei = 0
-        maior = 0
-        menor = 0
-        total = 0
-        quantidade = 0
-        n = str(input('Digite um numero ou S(sair) para ver os resultados: ').upper())
-        if n == 'S':
-            media = total / quantidade
-            print('A media de todos os numeros é {:.2f} o maior numero digitado foi {} é o menor {} '.format(media, maior, menor))
-            c= n
+primei = 0
+maior = 0
+menor = 0
+total = 0
+quantidade = 0
 
+repete = 0
+while repete == 0:
+    c = 0
+    while c == 0:
+        n = int (input('Digite um numero ou 0 para resultados: '))
+        if n == 0:
+            c += 1
         else:
-            n = int(n)
             primei +=1
             if primei == 1:
                 maior = n
             elif n > maior:
                 maior = n
-            if primei == 1:
+            elif primei == 1:
                 menor = n
             elif n < menor:
                 menor = n
-            total += n
-            quantidade += 1
+
+
+        total += n
+        quantidade += 1
+    media = total / quantidade
+    print('A media de todos os numeros é {:.2f} o maior numero digitado foi {} é o menor {} '.format(media, maior,menor))
+
+    repete = int(input('realizar novamente 0 ou 1 para sair: '))
 
 
 
-    f = str(input('Voce deseja continuar S/N').upper())
-    if f == 'N':
-        repete += f
+    #f = str(input('Voce deseja continuar S/N').upper())
+
 
 
 
