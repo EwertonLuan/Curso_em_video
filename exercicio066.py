f553a3394ce57229261b6a3590e183724d160f2d
Ewerton <ewerton.luan@outlook.com>
2018-01-10 13:34:55 -0200

    Exercicios

--- exercicio066.py
+++ exercicio066.py
@@ -2,10 +2,9 @@ total = 0
 soma = 0
 
 while True:
-    n = int(input('Digite um numero ou 999 para parar: '))
+    n = int(input('Digite um numero (999 para parar): '))
     if n == 999:
         break
-    else:
-        total += 1
-        soma += n
-print(f'Foram digitados {total} e a soma de todos ele é de {soma}')
+    total += 1
+    soma += n
+print('Foram digitados {} e a soma de todos ele é de {}'.format(total,soma))
--- exercicio071.py
+++ exercicio071.py
@@ -3,13 +3,32 @@ a_50 = 0
 b_20 = 0
 c_10 = 0
 d_1 = 0
+
 while True:
-    sacar = int(input('Quanto vc deseja sacar: '))
-    while sacar % 50 == 0:
+    sacar = int(input('Quanto vc deseja sacar: R$'))
+    while sacar >= 50:
         sacar = sacar - 50
         a_50 += 1
-        print(sacar)
 
-    print(a_50)
+    while sacar >= 20:
+        sacar = sacar - 20
+        b_20 += 1
+
+    while sacar >= 10:
+        sacar = sacar - 10
+        c_10 += 1
+
+    while sacar >= 1:
+        sacar = sacar - 1
+        d_1 += 1
     break
+
+if a_50 > 0:
+    print(f'total de celulas de 50 {a_50}')
+if b_20 >0:
+    print(f'total de celulas de 20 {b_20}')
+if c_10 >0:
+    print(f'total de celulas de 10 {c_10}')
+if d_1 >0:
+    print(f'total de celulas de 1 {d_1}')
 
